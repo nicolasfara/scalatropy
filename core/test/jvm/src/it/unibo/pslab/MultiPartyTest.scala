1c4baf073704a171be70dcfc9e81c2e87eacb259
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2026-05-12 15:17:08 +0200

    test: use mocked network

--- core/test/jvm/src/it/unibo/pslab/MultiPartyTest.scala
+++ core/test/jvm/src/it/unibo/pslab/MultiPartyTest.scala
@@ -9,23 +9,13 @@ import it.unibo.pslab.network.AnyProtocol
 import it.unibo.pslab.peers.Peers.*
 import it.unibo.pslab.network.*
 import cats.Id
-import it.unibo.pslab.multiparty.Environment.Reference
-import cats.data.NonEmptyList
 import cats.Monad
 import cats.syntax.all.*
+import org.scalamock.stubs.Stubs
 
 type PeerRef[P <: Peer] = String
-class TestNetwork[P <: Peer] extends Network[Id, P, PeerRef] {
-  override val local: PeerRef[P] = "local"
 
-  override def send[V: Encodable[Id], To <: Peer: PeerTag](value: V, resource: Reference, to: PeerRef[To]): Id[Unit] = ???
-
-  override def receive[V: Decodable[Id], From <: Peer: PeerTag](resource: Reference, from: PeerRef[From]): Id[V] = ???
-
-  override def alivePeersOf[RP <: Peer: PeerTag]: Id[NonEmptyList[PeerRef[RP]]] = ???
-}  
-
-class MultiPartyTest extends AnyFunSpec, should.Matchers:
+class MultiPartyTest extends AnyFunSpec, should.Matchers, Stubs:
   type Foo <: { type Tie <: via[AnyProtocol toMultiple Foo] }
   type Bar <: Foo
   type Baz <: { type Tie <: via[AnyProtocol toMultiple Foo] }
@@ -36,12 +26,12 @@ class MultiPartyTest extends AnyFunSpec, should.Matchers:
         def testProgram[F[_]: Monad](using MultiParty[F]) =
           var counter = 0
           for
-            _ <- on[Foo] { (counter += 1).pure } // Evaluated
-            _ <- on[Bar] { (counter += 1).pure } // Evaluated, since Bar <: Foo
-            _ <- on[Baz] { (counter += 1).pure } // Not evaluated, since Baz is not a subtype of Foo or Bar
-          yield 
+            _ <- on[Foo] { (counter += 1).pure }
+            _ <- on[Bar] { (counter += 1).pure }
+            _ <- on[Baz] { (counter += 1).pure }
+          yield
             counter shouldBe 2
             ()
-
+        val network = stub[Network[Id, Bar, PeerRef]]
         ScalaTropy(testProgram[Id]).projectedOn[Bar]:
-          tiedTo[Foo] via TestNetwork[Bar]()
+          tiedTo[Foo] via network
